445c28c245074288318ea89936e661fa7d871513
Bernice Boateng <berniceboateng775@gmail.com>
2026-03-26 10:52:43 +0000

    modular project structure with fastapi routers

---
+++ src/books/book_data.py
@@ -0,0 +1,47 @@
+books = [ 
+    {
+        "id": 1,
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald",
+        "publisher": "Scribner",
+        "published_date": "1925-04-10",
+        "page_count" : 1234,
+        "language": "English"
+    },
+    {
+        "id": 2,
+        "title": "To Kill a Mockingbird",
+        "author": "Harper Lee",
+        "publisher": "J. B. Lippincott & Co.",
+        "published_date": "1960-07-11",
+        "page_count" : 281,
+        "language": "English"
+    },
+    {
+        "id": 3,
+        "title": "Ruthless People",
+        "author": "J.J McAvoy",
+        "publisher": "J.J McAvoy",
+        "published_date": "2015-05-10",
+        "page_count" : 328,
+        "language": "English"
+    },
+    {
+        "id": 4,
+        "title": "The Catcher in the Rye",
+        "author": "J. D. Salinger",
+        "publisher": "Little, Brown and Company",
+        "published_date": "1951-07-16",
+        "page_count" : 277,
+        "language": "English"
+    },
+    {
+        "id": 5,
+        "title": "Handsome Devil",
+        "author": "L.J Shen",
+        "publisher": "L.J Shen",
+        "published_date": "2016-05-10",
+        "page_count" : 300,
+        "language": "English"
+    }
+]
